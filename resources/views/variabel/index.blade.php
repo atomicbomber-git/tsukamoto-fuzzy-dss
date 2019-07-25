b1c88fb8cb1ed9827f85e6eebfbb04e56cc55cb2
Antonius Yonathan <atomicbomber.email@gmail.com>
2019-07-25 15:06:37 +0700

    Progress.

--- resources/views/variabel/index.blade.php
+++ resources/views/variabel/index.blade.php
@@ -2,25 +2,128 @@
 @section('title', 'Kelola Variabel')
 @section('content')
 
-<div>
-    <div class="w-3/6 mx-auto mt-5">
-        <h1 class="text-6xl text-red-800 font-bold sticky top-0">
-            Big Ass Text
-        </h1>
+<div class="md:flex md:w-3/4 md:mx-auto">
+    <div id="sidebar" class="hidden bg-red-900 text-gray-100 md:block md:w-3/12 pt-5">
+        <div class="bg-blue-900 md:sticky md:top-0 px-3">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+        </div>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium cupiditate, ad reprehenderit optio, iure quia quas dolores similique commodi vero et autem in. Possimus distinctio tempore rem minus blanditiis accusantium?
-        </p>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quam quos culpa modi necessitatibus reprehenderit repellat rem non, tempora, at omnis, aliquid aperiam numquam sit minus? Alias consectetur corporis nihil?
-        </p>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit obcaecati impedit incidunt numquam labore architecto dolor delectus minima illum assumenda, consequatur quo voluptatem nihil tempore ex cumque repellat sed inventore.
-        </p>
     </div>
+
+    <div class="bg-green-900 text-gray-100 md:w-9/12 pt-5">
+        <div class="px-3">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed fugiat tempora asperiores dolores, tenetur natus quo corrupti dolor? Corporis ipsum cupiditate tempore dolor maxime nobis eligendi! Earum, magni inventore.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga similique rem asperiores quibusdam officia neque. Dolorem, nihil. Debitis consequuntur ab repellat fugit beatae doloremque laboriosam sit magnam rem quidem?
+            </p>
+        </div>
+    </div>
+
+    <script>
+        let menuCollapseButton = document.querySelector("#collapse-menu")
+        let sidebar = document.querySelector("#sidebar")
+
+        menuCollapseButton.addEventListener('click', function() {
+            if (sidebar.classList.contains("hidden")) {
+                sidebar.classList.remove("hidden")
+            }
+            else {
+                sidebar.classList.add("hidden")
+            }
+        })
+    </script>
 </div>
-
-
 @endsection
